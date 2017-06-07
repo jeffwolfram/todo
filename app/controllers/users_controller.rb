9553827779dbd0014bde4918d4e049265ea91af1
Jeff Wolfram <jeffwolfram@gmail.com>
2017-06-06 22:10:33 -0700

    fixed the user destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ def create
   @user.name = params[:user][:name]
   @user.email = params[:user][:email]
   @user.password = params[:user][:password]
-  @user.password_confirmation = params[:user][:password_confirmation]
+  # @user.password_confirmation = params[:user][:password_confirmation]
   if @user.save
     flash[:notice] = "Welcome to Bloccit #{@user.name}!"
     create_session(@user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-has_many :lists, dependent: :destroy
+# has_many :lists, dependent: :destroy
 
   before_save {self.email = email.downcase if email.present?}
 
